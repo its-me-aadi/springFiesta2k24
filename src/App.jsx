e5da4d19ef8ade44ff5e11c25b569538fe634b79
yashsojitra97 <yashsojitra97@gmail.com>
2024-04-01 22:05:42 +0530

    some css change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route , Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './screens/Home';
+import CoreTeam from './screens/CoreTeam';
 import EventPage from './screens/EventPage';
 
 function App() {
@@ -9,7 +11,9 @@ function App() {
       <Router>
         <Navbar/>
         <Routes>
+          <Route path="/" element={<Home/>}></Route>
           <Route path="/events" element={<EventPage/>}></Route>
+          <Route path="/core-team" element={<CoreTeam/>}></Route>
         </Routes>
       </Router>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,42 @@
-import React from 'react'
-import '../styles/navbar.css'
-import { Link, useLocation } from 'react-router-dom'
-
+import React from "react";
+import "../styles/navbar.css";
+import { Link, useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
 const Navbar = () => {
-    const location = useLocation()
-    return (
-        <div className='nav-main'>
-            <ul className="nav-ul">
-                <li className="nav-li">
-                    <Link to='/'>Home</Link>
-                    {location.pathname === '/' && <div className="border-cross"></div>}
-                </li>
-                <li className="nav-li">
-                    <Link to='/events'>Event Schedule</Link>
-                    {location.pathname === '/events' && <div className="border-cross"></div>}
-                </li>
-                <li className="nav-li">
-                    <Link to='/core'>Core Members</Link>
-                    {location.pathname === '/core' && <div className="border-cross"></div>}
-                </li>
-                <li className="nav-li">
-                    <Link to='/voting'>Voting Page</Link>
-                    {location.pathname === '/voting' && <div className="border-cross"></div>}
-                </li>
-            </ul>
-        </div>
-    )
-}
+  const location = useLocation();
+  return (
+    <motion.div
+      className="nav-main"
+      initial={{ y: -100, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ ease: "easeIn", duration: 0.6 }}
+    >
+      <ul className="nav-ul">
+        <li className="nav-li">
+          <Link to="/">Home</Link>
+          {location.pathname === "/" && <div className="border-cross"></div>}
+        </li>
+        <li className="nav-li">
+          <Link to="/events">Event Schedule</Link>
+          {location.pathname === "/events" && (
+            <div className="border-cross"></div>
+          )}
+        </li>
+        <li className="nav-li">
+          <Link to="/core-team">Core Members</Link>
+          {location.pathname === "/core-team" && (
+            <div className="border-cross"></div>
+          )}
+        </li>
+        <li className="nav-li">
+          <Link to="/voting">Voting Page</Link>
+          {location.pathname === "/voting" && (
+            <div className="border-cross"></div>
+          )}
+        </li>
+      </ul>
+    </motion.div>
+  );
+};
 
-export default Navbar
+export default Navbar;
