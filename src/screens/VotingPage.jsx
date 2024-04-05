db6af2f8a3c0cb16e6f2f290075a29beecdc09c7
Suvrat Bajpai <suvratbajpai337@gmail.com>
2024-04-05 20:17:00 +0530

    Voting page Updated

--- src/screens/VotingPage.jsx
+++ src/screens/VotingPage.jsx
@@ -1,19 +1,21 @@
-import React, {useEffect, useState} from 'react';
-import '../styles/VotingPage.css'
+import React, { useEffect, useState } from 'react';
+import '../styles/VotingPage.css';
 import axios from 'axios';
 import Navbar from '../components/Navbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import party_pooper1 from '../components/partyPooper1.svg'
-import party_pooper2 from '../components/partyPooper2.svg'
-import sprinklers1 from '../components/sprinklers1.svg'
-import sprinklers2 from '../components/sprinklers2.svg'
-import circus_tent from '../components/circusTent.svg'
+import party_pooper1 from '../components/partyPooper1.svg';
+import party_pooper2 from '../components/partyPooper2.svg';
+import sprinklers1 from '../components/sprinklers1.svg';
+import sprinklers2 from '../components/sprinklers2.svg';
+import circus_tent from '../components/circusTent.svg';
 import images from "../image.json";
 
 function VotingPage() {
 
-  console.log(images);
+  const handleLikeClick = (imageId) => {
+    console.log("Liked image with ID:", imageId);
+  };
 
   return (
     <div className='main-body'>
@@ -28,23 +30,35 @@ function VotingPage() {
         </a>
       </div>
       <div className='sprinklers'>
-          <img src= {sprinklers1} className="sprinklers1" alt="sprinklers1" />
-          <img src= {sprinklers2} className="sprinklers2" alt="sprinklers2" />
+        <img src={sprinklers1} className="sprinklers1" alt="sprinklers1" />
+        <img src={sprinklers2} className="sprinklers2" alt="sprinklers2" />
       </div>
       <div className="box">
-         {images.map((image)=>{
-           return (
-             <div className="box-container1">
-                <img src={image.image} className="image" alt="image1" />
-                <img src={image.like} className="like-button" alt="like" />
+        {images.map((image) => {
+          return (
+            <div className="box-container">
+              <div className="main-image">
+                <img src={image.image} alt="image1" />
               </div>
-            )
-          })}
+              <div className='container-details'>
+                <text className='name'>{image.name}</text>
+                <div className='voting-count'>
+                  <button className='like-btn' onClick={() => handleLikeClick(image.id)} >
+                    <div className="like-img">
+                      <img src={image.like} alt="Like" />
+                    </div>
+                    <h4>23</h4>
+                  </button>
+                </div>
+              </div>
+            </div>
+          )
+        })}
       </div>
       <div className='canvas'>
-          <img src= {party_pooper1} className="party-pooper1" alt="party-pooper1" />
-          <img src= {circus_tent}className="circus" alt="circus" />
-          <img src= {party_pooper2} className="party-pooper2" alt="party-pooper2" />
+        <img src={party_pooper1} className="party-pooper1" alt="party-pooper1" />
+        <img src={circus_tent} className="circus" alt="circus" />
+        <img src={party_pooper2} className="party-pooper2" alt="party-pooper2" />
       </div>
     </div>
   );
